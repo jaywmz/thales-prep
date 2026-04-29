a3c0ce145e53d0b36eea1acf6c512b296173e4d6
jaywmz <lichtwm@hotmail.com>
2026-04-29 19:26:07 +0800

    Update application to use port 8080 instead of 5000 in playbook, Dockerfile, and app.py

--- terraform/main.tf
+++ terraform/main.tf
@@ -86,8 +86,8 @@ resource "aws_security_group" "main" {
 
   ingress {
     description = "App port"
-    from_port   = 5000
-    to_port     = 5000
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
