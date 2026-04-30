e0c504a6780db47cc25075e316cc35bda7728b74
jaywmz <lichtwm@hotmail.com>
2026-04-30 18:13:00 +0800

    Update security group to allow SSH access from any IP address

--- terraform/main.tf
+++ terraform/main.tf
@@ -73,7 +73,7 @@ resource "aws_security_group" "main" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["222.164.4.213/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
